190f18ca146c2b06c546a0a0f5377bab830c7ac9
Aswin <153578641+workufstechnologies@users.noreply.github.com>
2026-01-24 17:57:46 +0530

    feat: Implement initial admin panel, core application structure, and shared components.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    BasePath:"http://localhost:3520/",
-    FilePath:"http://localhost:3520/",
+    BasePath:"http://localhost:3515/",
+    FilePath:"http://localhost:3515/",
 //  BasePath:"https://app.breffniacademy.in/",
 //  BasePath:"https://lmsapi.ufstech.in/",
 
