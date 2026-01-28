941f09b444b98fe61df14c7ebc9982ead2bb5bb6
AshwiniSuresh12 <ufstechgithub@gmail.com>
2026-01-28 17:02:35 +0530

    update course page

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     BasePath:"http://localhost:3515/",
-    FilePath:"http://localhost:3515/",
+    FilePath:"https://pub-11714a99f3bd420ca95f23dda2af714b.r2.dev/",
 //  BasePath:"https://app.breffniacademy.in/",
 //  BasePath:"https://lmsapi.ufstech.in/",
 
